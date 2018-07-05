45160f1ce1ce136b490008df20492b576a98886a
Thierry GUENNOU <31097359+mrguen@users.noreply.github.com>
2018-07-05 10:36:57 +0200

    Add files via upload

--- src/fonts/allFonts.h
+++ src/fonts/allFonts.h
@@ -1,11 +1,11 @@
 /*
- * allFonts.h font header for GLCD library 
+ * allFonts.h font header for GLCD library
  * The fonts listed below will be available in a sketch if this file is included
  *
  * If you create your own fonts you can add the header to this file
  *
- * Note that the build environment only holds a font in Flash if its selected 
- * so there is no penalty to including a font file here if its not used 
+ * Note that the build environment only holds a font in Flash if its selected
+ * so there is no penalty to including a font file here if its not used
  */
 
 #ifndef _allFonts_h_
@@ -24,20 +24,20 @@
 #endif  // __AVR__
 //------------------------------------------------------------------------------
 // Font Indices
-#define FONT_LENGTH      0
-#define FONT_WIDTH       2
-#define FONT_HEIGHT      3
-#define FONT_FIRST_CHAR  4
-#define FONT_CHAR_COUNT  5
-#define FONT_WIDTH_TABLE 6
-// 
+#define FONT_LENGTH			  0
+#define FONT_FIXED_WIDTH	2
+#define FONT_HEIGHT			  3
+#define FONT_FIRST_CHAR		4
+#define FONT_CHAR_COUNT		5
+#define FONT_WIDTH_TABLE	6
+//
 // FONT_LENGTH is a 16 bit Big Endian length field.
 // Unfortunately, FontCreator2 screwed up the value it put in the field
 // so it is pretty much meaningless. However it still is used to indicate
 // some special things.
 // 00 00 (fixed width font with 1 padding pixel on right and below)
 // 00 01 (fixed width font with no padding pixels)
-// FONT_WIDTH it the max character width. 
+
 // any other value means variable width font in FontCreator2 (thiele)
 // format with pixel padding
 
@@ -82,6 +82,8 @@
 #include "fixednums15x31.h"      // fixed width font - + , - . / [0-9] and :
 
 #include "CalBlk36.h"
+#include "CalBlk36_shortPoint.h" // added TG 05.07.2018
+
 #include "CalLite24.h"
 #include "lcdnums12x16.h"	// font that looks like LCD digits
 #include "lcdnums14x24.h"	// font that looks like LCD digits
